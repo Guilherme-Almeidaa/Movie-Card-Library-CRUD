34a8867262dc55ecc8f21f7462b672ed660a2fae
Guilherme <almeidaguilherme204@gmail.com>
2021-01-15 20:36:37 -0300

    Testando-windows

--- Trybe-Wallet/src/pages/Wallet.js
+++ Trybe-Wallet/src/pages/Wallet.js
@@ -9,7 +9,7 @@ class Wallet extends React.Component {
  
   render() {
     return (
-      <div className="header-complete"  >
+      <div className='header-complete'  >
       <HeaderWallet />
       <FormDespesas />
       <TableInfo/>
