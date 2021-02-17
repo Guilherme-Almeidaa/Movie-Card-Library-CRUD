284faff1a5998d866f6a312cba39c4a0b91e2957
Guilherme Almeida <almeidaguilherme204@gmail.com>
2021-02-17 09:47:51 -0300

    Add

---
+++ Movie Card Library CRUD/src/pages/NewMovie.js
@@ -0,0 +1,33 @@
+import { Redirect } from 'react-router-dom';
+import React, { Component } from 'react';
+import MovieForm from '../components/MovieForm';
+import * as movieAPI from '../services/movieAPI';
+
+class NewMovie extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirect: false,
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  async handleSubmit(newMovie) {
+    this.setState({
+      redirect: true,
+    });
+    await movieAPI.createMovie(newMovie);
+  }
+
+  render() {
+    const { redirect } = this.state;
+
+    if (redirect) return <Redirect to="/" />;
+    return (
+      <div data-testid="new-movie">
+        <MovieForm onSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+export default NewMovie;
